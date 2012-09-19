035cc49a183f28f9e1fdaa9708bbb39b3222655c
Kishore Sajja <sajja.kishore@gmail.com>
2012-09-19 16:39:41 -0400

    Linked UI to Actaul Cash Register.

--- CashReegister/ViewController.m
+++ CashReegister/ViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "KSTransaction.h"
+#import "KSCashRegister.h"
 
 @interface ViewController ()
-
+@property(nonatomic, retain) NSNumberFormatter *formatter;
+@property(nonatomic, retain) KSTransaction *transaction;
 @end
 
 @implementation ViewController
@@ -17,9 +20,12 @@
 @synthesize cashHanded;
 @synthesize purchasePriceLabel;
 @synthesize cashHandedLabel;
+@synthesize formatter;
+@synthesize transaction;
 
 - (void)viewDidLoad
 {
+    self.title = @"Cash Register";    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
@@ -34,6 +40,14 @@
     // Release any retained subviews of the main view.
 }
 
+-(void) viewWillAppear:(BOOL)animated
+{
+    //Initial Setup.
+    self.purchasePriceLabel.hidden =  self.cashHandedLabel.hidden = YES;
+    self.purchasePrice.text = @"";
+    self.cashHanded.text = @"";    
+    [super viewWillAppear:animated];
+}
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (UIInterfaceOrientationIsPortrait(interfaceOrientation));
@@ -42,6 +56,39 @@
 #pragma mark - Interface Builder Actions
 - (IBAction)runTransaction:(id)sender 
 {
+    [self.cashHanded resignFirstResponder];
+    [self.purchasePrice resignFirstResponder];
+    NSNumber *pp = [self.formatter numberFromString:self.purchasePrice.text];   
+    if (!pp)
+        pp = [NSNumber numberWithDouble:self.purchasePrice.text.doubleValue];
+    
+    NSNumber *ch = [self.formatter numberFromString:self.cashHanded.text];
+    if(!ch)
+        ch = [NSNumber numberWithDouble:self.cashHanded.text.doubleValue];
+    
+    if(self.cashHanded.text.length == 0 || self.purchasePrice.text.length == 0)
+    {
+        [[[UIAlertView alloc] initWithTitle:@"Error" message:@"Please enter both purchase price and cash handed by the customer." delegate:nil cancelButtonTitle:@"Dismiss" otherButtonTitles: nil] show];
+        return;
+    }
+    
+    @try 
+    {
+        KSTransaction *txn = [KSCashRegister transactionWithCashHanded:ch.floatValue andPurchasePrice:pp.floatValue];
+        if (txn.output.count == 0) 
+        {
+            [[[UIAlertView alloc] initWithTitle:@"Error" message:@"Error running your transaction. Please check check the input and try again." delegate:nil cancelButtonTitle:@"Dismiss" otherButtonTitles: nil] show];
+        }
+        else 
+        {
+            self.transaction = txn;
+            NSLog(@"%@",txn.output);
+        }
+    }
+    @catch (NSException *exception) 
+    {
+        [[[UIAlertView alloc] initWithTitle:@"Error" message:@"Error running your transaction. Please check check the input and try again." delegate:nil cancelButtonTitle:@"Dismiss" otherButtonTitles: nil] show];
+    }
     
 }
 
