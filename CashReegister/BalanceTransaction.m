d067b06137f7b0f4839574d2db8dcd59ac48399b
Venkateshwara Rao <raorepalle@gmail.com>
2012-10-09 18:20:28 -0400

    Balance calculation completed. Simple test case written for testing

--- CashReegister/BalanceTransaction.m
+++ CashReegister/BalanceTransaction.m
@@ -11,7 +11,7 @@
 #import "TransactionUtil.h"
 
 @interface BalanceTransaction()
-@property(nonatomic,strong) NSMutableArray* cashDenomination;
+@property(nonatomic,strong) NSArray* cashDenomination;
 @end
 
 @implementation BalanceTransaction
@@ -36,14 +36,14 @@
                 [array addObject:[[Denomination alloc]  initWithTitle:title andValue :value ]];
                  NSLog(@"Val %@",value);
             }];
-            NSLog(@"Array %@",array);
+            NSLog(@"Denomination Array %@",array);
             //Sort the array based upon currency value
             [array sortUsingComparator:^NSComparisonResult(Denomination *obj1, Denomination *obj2) {
                 return [obj2.value compare :obj1.value ];
             }];
             
-            NSLog(@"Array %@",array);
-            self.cashDenomination = array;
+            NSLog(@"Sorted Array %@",array);
+            self.cashDenomination = [array copy];
         }
         
         return self;
@@ -52,12 +52,50 @@
     return nil;
 }
 
-
+//Calculate the balance and convert into denominations
 -(BalanceOutput*) calculateBalanceWithPrice:(NSString*) pPrice andCachReceived : (NSString*) cachReceived
 {
-    BalanceOutput* bOUtput = [[BalanceOutput alloc] init];
+    BalanceOutput* trBalance = [[BalanceOutput alloc] init];
+    //Converting string into NSDecimal Number
+    NSDecimalNumber* purchasePrice = [TransactionUtil convertToDecimalFromString:pPrice];
+    NSDecimalNumber* cReceived = [TransactionUtil convertToDecimalFromString:cachReceived];
     
-    return bOUtput;
+    //checking both purchasePrice and Cash Received are same or not.
+    if([cReceived compare:purchasePrice] == 0)
+        return trBalance;
+    
+    //Checking whether Cash Received is greater than purchase price.
+    if([cReceived compare:purchasePrice] < 0)
+    {
+        trBalance.isError = YES;
+        trBalance.balance = [NSDecimalNumber zero];
+        return trBalance;
+    }
+    //Calculating the change
+    NSDecimalNumber* change = [cReceived decimalNumberBySubtracting:purchasePrice withBehavior:[TransactionUtil roundingHandler]];
+    trBalance.balance = change;
+    
+    NSMutableArray* output = [NSMutableArray array];
+    NSDecimalNumber* balance = [change copy];
+    NSArray* copyDenominations = [self.cashDenomination copy];
+
+    //Converting the change into Denominations
+    for(Denomination* denomination in copyDenominations)
+    {
+        NSLog(@"Balance is %f",[balance floatValue]);
+        NSLog(@"%@ Currrency %f count %i", denomination.title,[denomination.value floatValue],denomination.count);
+        if([balance compare:denomination.value ] >= 0)
+        {
+            
+            denomination.count = [[balance decimalNumberByDividingBy: denomination.value withBehavior:[TransactionUtil roundingHandler]] intValue];
+            [output addObject:denomination];
+            balance = [balance decimalNumberBySubtracting:[denomination.value decimalNumberByMultiplyingBy:[[NSDecimalNumber alloc] initWithInt: denomination.count]]];
+            if([balance isEqual:[NSDecimalNumber zero]])
+                break;
+        }
+    }
+    trBalance.output = [output copy];
+    return trBalance;
 }
 
 @end
--- CashReegisterTests/CashReegisterTests.m
+++ CashReegisterTests/CashReegisterTests.m
@@ -7,26 +7,52 @@
 //
 
 #import "CashReegisterTests.h"
+#import "BalanceTransaction.h"
+#import "BalanceOutput.h"
+#import "TransactionUtil.h"
+#import "Denomination.h"
+
+@interface CashReegisterTests()
+
+@property(nonatomic,strong) BalanceTransaction* cashTransaction;
+@end
 
 @implementation CashReegisterTests
+@synthesize cashTransaction;
 
 - (void)setUp
 {
     [super setUp];
     
-    // Set-up code here.
+    self.cashTransaction = [[BalanceTransaction alloc] init];
 }
 
+-(void) testTransaction
+{
+    //343.67
+    BalanceOutput* balance = [self.cashTransaction calculateBalanceWithPrice:@"656.33" andCachReceived:@"1000"];
+    NSDecimalNumber *change1 = [NSDecimalNumber zero];
+    for(Denomination* obj in balance.output)
+    {
+        NSDecimalNumber *temp = [obj.value decimalNumberByMultiplyingBy:[[NSDecimalNumber alloc] initWithInt:obj.count ]  withBehavior:[TransactionUtil roundingHandler] ];
+        NSLog(@"%f temp value",[temp floatValue]);
+        change1 = [change1 decimalNumberByAdding:temp  withBehavior:[TransactionUtil roundingHandler] ];
+        
+        NSLog(@"%f value",[change1 floatValue]);
+    }
+    BalanceOutput* outPutBalance = [[BalanceOutput alloc] init];
+    
+    outPutBalance.balance = [NSDecimalNumber decimalNumberWithString:@"343.67"];
+    
+    STAssertTrue(([outPutBalance.balance compare: change1] == NSOrderedSame),@"Transaction failed.");
+}
 - (void)tearDown
 {
-    // Tear-down code here.
+    [self setCashTransaction:nil];
     
     [super tearDown];
 }
 
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in CashReegisterTests");
-}
+
 
 @end
