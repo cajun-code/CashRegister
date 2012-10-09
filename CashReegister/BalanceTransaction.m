4227b1ea12a0a0bbbbbed2c342457790b92a60d9
Venkateshwara Rao <raorepalle@gmail.com>
2012-10-09 17:57:53 -0400

    Loading data from the property list

--- CashReegister/BalanceTransaction.m
+++ CashReegister/BalanceTransaction.m
@@ -7,8 +7,50 @@
 //
 
 #import "BalanceTransaction.h"
+#import "Denomination.h"
+#import "TransactionUtil.h"
+
+@interface BalanceTransaction()
+@property(nonatomic,strong) NSMutableArray* cashDenomination;
+@end
 
 @implementation BalanceTransaction
+
+@synthesize cashDenomination;
+
+-(id) init
+{
+    if(self = [super init])
+    {
+        @synchronized(self)
+        {
+            //We need to make sure this file is available. 
+            NSString *path = [[NSBundle mainBundle] pathForResource:@"Denominations" ofType:@"plist"];
+            
+            // Load the file content and read the data into arrays
+            NSDictionary *denominations = [[NSDictionary alloc] initWithContentsOfFile:path];
+            NSMutableArray *array = [NSMutableArray array];
+            
+            //loads the property list into Array
+            [ denominations enumerateKeysAndObjectsUsingBlock:^(NSString* title, NSString* value, BOOL *stop) {
+                [array addObject:[[Denomination alloc]  initWithTitle:title andValue :value ]];
+                 NSLog(@"Val %@",value);
+            }];
+            NSLog(@"Array %@",array);
+            //Sort the array based upon currency value
+            [array sortUsingComparator:^NSComparisonResult(Denomination *obj1, Denomination *obj2) {
+                return [obj2.value compare :obj1.value ];
+            }];
+            
+            NSLog(@"Array %@",array);
+            self.cashDenomination = array;
+        }
+        
+        return self;
+    }
+    
+    return nil;
+}
 
 
 -(BalanceOutput*) calculateBalanceWithPrice:(NSString*) pPrice andCachReceived : (NSString*) cachReceived
