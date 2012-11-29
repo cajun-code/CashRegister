bfcfca1e1aadf4e1b71a78b134a04b2c43865de9
Mark Rusnak <admin@stock-events.com>
2012-11-29 09:21:48 -0500

    Change Calculation
    Just need to test some more...

--- CashReegister/ViewController.m
+++ CashReegister/ViewController.m
@@ -13,7 +13,8 @@
 // model
 @property (nonatomic) float purchasePrice;
 @property (nonatomic) float paymentAmount;
-@property (nonatomic, strong) NSMutableArray *cashRegisterCurrencyArray;
+@property (nonatomic, strong) NSMutableArray *changeArray;
+@property (nonatomic, strong) NSMutableDictionary *currencyDictionary;
 
 // ui
 @property (nonatomic, weak) IBOutlet UITextField *purchasePriceTextField;
@@ -23,21 +24,33 @@
 
 - (NSString *)validatePaymentAmount;
 - (BOOL)updateButtonStatusForPurchasePrice:(float)price andPaymentAmount:(float)payment;
+- (void)setupCurrencies;
+- (NSString *)findLargestCurrencyForAmount:(float)amount;
 
 @end
 
 @implementation ViewController
 
-@synthesize cashRegisterCurrencyArray;
+// model
 @synthesize purchasePrice;
 @synthesize paymentAmount;
+@synthesize changeArray;
+@synthesize currencyDictionary;
+
+// ui
+@synthesize purchasePriceTextField;
+@synthesize paymentAmountTextField;
+@synthesize calculateButton;
+@synthesize resultLabel;
 
 #pragma mark - === VIEW CONTROLLER LIFECYCLE === -
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self setupCurrencies];
     [[self purchasePriceTextField] setTag:0];
     [[self paymentAmountTextField] setTag:1];
+    changeArray = [[NSMutableArray alloc] init];
 }
 
 - (void)viewDidUnload
@@ -78,6 +91,7 @@
 - (IBAction)distributeChange:(id)sender
 {
     NSLog(@"distributeChange");
+    [changeArray removeAllObjects];
 
     [[self view] endEditing:YES];
     [[self resultLabel] setTextAlignment:NSTextAlignmentLeft];
@@ -107,6 +121,37 @@
         float change = paymentAmount - purchasePrice;
 
         NSLog(@"change: %.2f", change);
+        
+        float runningChange = change;
+        
+        while (runningChange > 0)
+        {
+            
+            NSString *currencyString = [self findLargestCurrencyForAmount:runningChange];
+            
+            float currencyValue = [[currencyDictionary valueForKey:currencyString] floatValue];
+            
+            if (currencyValue > 0)
+            {
+                runningChange = runningChange - currencyValue;
+                [changeArray addObject:currencyString];
+            }
+            else
+            {
+                // safety check
+                break;
+            }
+        }
+
+        // sort result array alphabetically
+        NSArray *resultArray = (NSMutableArray *)[changeArray sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+
+        // display string
+        NSString *displayString = [[resultArray valueForKey:@"description"] componentsJoinedByString:@","];
+        //NSLog(@"%@", displayString);
+
+        [[self resultLabel] setTextColor:[UIColor blackColor]];
+        [[self resultLabel] setText:displayString];
     }
 }
 
@@ -116,22 +161,89 @@
     // reset message field
     [[self resultLabel] setText:@""];
 
-    NSString *amount = [textField.text stringByReplacingCharactersInRange:range withString:string];
-    
-    // update float values
-    if ([textField tag] == 0)
+    // limit to numbers and decimal point
+    NSCharacterSet *nonNumberSet = [[NSCharacterSet characterSetWithCharactersInString:@"0123456789."] invertedSet];
+
+    // handle backspace
+    if ([string length] == 0)
     {
-        purchasePrice = [amount floatValue];
+        NSString *amount = [textField.text stringByReplacingCharactersInRange:range withString:string];
+        
+        // update float values
+        if ([textField tag] == 0)
+        {
+            purchasePrice = [amount floatValue];
+        }
+        else if ([textField tag] == 1)
+        {
+            paymentAmount = [amount floatValue];
+        }
+
+        return YES;
     }
-    else if ([textField tag] == 1)
+    else if ([string stringByTrimmingCharactersInSet:nonNumberSet].length > 0)
     {
-        paymentAmount = [amount floatValue];
+        NSString *amount = [textField.text stringByReplacingCharactersInRange:range withString:string];
+
+        // update float values
+        if ([textField tag] == 0)
+        {
+            purchasePrice = [amount floatValue];
+        }
+        else if ([textField tag] == 1)
+        {
+            paymentAmount = [amount floatValue];
+        }
+
+        //BOOL isButtonEnabled = [self updateButtonStatusForPurchasePrice:purchasePrice andPaymentAmount:paymentAmount];
+        //[[self calculateButton] setEnabled:isButtonEnabled];
+
+        return YES;
     }
 
-    //BOOL isButtonEnabled = [self updateButtonStatusForPurchasePrice:purchasePrice andPaymentAmount:paymentAmount];
-    //[[self calculateButton] setEnabled:isButtonEnabled];
+    return NO;
+}
 
-    return YES;
+- (void)setupCurrencies
+{
+    currencyDictionary = [[NSMutableDictionary alloc] init];
+
+    [currencyDictionary setValue:[NSNumber numberWithFloat:0.01f] forKey:@"PENNY"];
+    [currencyDictionary setValue:[NSNumber numberWithFloat:0.05f] forKey:@"NICKEL"];
+    [currencyDictionary setValue:[NSNumber numberWithFloat:0.10f] forKey:@"DIME"];
+    [currencyDictionary setValue:[NSNumber numberWithFloat:0.25f] forKey:@"QUARTER"];
+    [currencyDictionary setValue:[NSNumber numberWithFloat:0.50f] forKey:@"HALF DOLLAR"];
+    [currencyDictionary setValue:[NSNumber numberWithFloat:1.00f] forKey:@"ONE"];
+    [currencyDictionary setValue:[NSNumber numberWithFloat:2.00f] forKey:@"TWO"];
+    [currencyDictionary setValue:[NSNumber numberWithFloat:5.00f] forKey:@"FIVE"];
+    [currencyDictionary setValue:[NSNumber numberWithFloat:10.00f] forKey:@"TEN"];
+    [currencyDictionary setValue:[NSNumber numberWithFloat:20.00f] forKey:@"TWENTY"];
+    [currencyDictionary setValue:[NSNumber numberWithFloat:50.00f] forKey:@"FIFTY"];
+    [currencyDictionary setValue:[NSNumber numberWithFloat:100.00f] forKey:@"ONE HUNDRED"];
+}
+
+- (NSString *)findLargestCurrencyForAmount:(float)amount
+{
+    // sort by largest amount
+    NSArray *sortedValuesArray = [currencyDictionary keysSortedByValueUsingSelector:@selector(compare:)];
+    sortedValuesArray = [[sortedValuesArray reverseObjectEnumerator] allObjects];
+
+    for (int i=0; i < [sortedValuesArray count]; i++)
+    {
+        float currencyAmount = [[currencyDictionary valueForKey:[sortedValuesArray objectAtIndex:i]] floatValue];
+        NSLog(@"%.2f", amount - currencyAmount);
+        
+        // account for rounding error potential
+        int currencyAmountInteger = currencyAmount * 100;
+        int amountInteger = amount * 100;
+        
+        if (amountInteger - currencyAmountInteger >= 0)
+        {
+            return [sortedValuesArray objectAtIndex:i];
+        }
+    }
+
+    return @"";
 }
 
 // for future use to enable and disable button
