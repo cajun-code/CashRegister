2d2a66e4d6a317bb871f43241b3176de2e77d1b5
raghu <raghu@DESKTOP-ALR6MIE.attlocal.net>
2021-07-14 00:55:15 -0400

    Updated DgController

--- src/com/dg/controller/DgController.java
+++ src/com/dg/controller/DgController.java
@@ -4,7 +4,7 @@ public class DgController {
 	
 	public static void main(String[] args) {
 		System.out.println("Inside main method of DgController");
-		System.out.println("Date = "+new Date());
+		System.out.println("Date = "+new java.util.Date());
 	}
 
 }
